0ec0b54f14a10315a741290239f5b93f9512b42b
BubblyJellyCat <BubblyJellyCat@gmail.com>
2024-04-20 21:45:41 +0300

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tema6Forms
+{
+    public partial class Form1 : Form
+    {
+        int raspunsCorect;
+        int nrIntreb = 1;
+        int scor;
+        int procent;
+        int intrebariTotale;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            VerificareRaspunsCorect(nrIntreb);
+            intrebariTotale = 6;
+        }
+
+        private void VerificareRaspunsForms(object sender, EventArgs e)
+        {
+            var objectSender = (Button)sender;
+
+            int buttonTag = Convert.ToInt32((objectSender).Tag);
+
+            if (buttonTag == raspunsCorect)
+            {
+                scor++;
+            }
+            if (nrIntreb == intrebariTotale)
+            {
+                button1.Show();
+                button3.Show();
+                nrIntreb = 0;
+                scor = 0;
+            }
+
+            nrIntreb++;
+            VerificareRaspunsCorect(nrIntreb);
+        }
+
+        private void VerificareRaspunsCorect(int nrIntreb)
+        {
+            switch (nrIntreb) 
+            {
+                case 1:
+                    label1.Text = "Intrebarea numarul 1: ";
+                    label2.Text = "Care este cel mai mare animal?";
+                    button1.Text = "Elefantul";
+                    button2.Text = "Soimul";
+                    button3.Text = "Girafa";
+
+                    raspunsCorect = 1;
+                    
+                    break;
+                case 2:
+                    label1.Text = "Intrebarea numarul 2: ";
+                    label2.Text = "Care este cel mai rapid animal?";
+                    button1.Text = "Leul";
+                    button2.Text = "Ghepardul";
+                    button3.Text = "Pisica";
+
+                    raspunsCorect = 2;
+
+                    break;
+                case 3:
+                    label1.Text = "Intrebarea numarul 3: ";
+                    label2.Text = "Care este cel mai incet animal?";
+                    button1.Text = "Elefantul";
+                    button2.Text = "Girafa";
+                    button3.Text = "Melcul";
+
+                    raspunsCorect = 3;
+
+                    break;
+                case 4:
+                    label1.Text = "Intrebarea numarul 4: ";
+                    label2.Text = "Ce animal face Miau?";
+                    button1.Text = "Pisica";
+                    button2.Text = "Cainele";
+                    button3.Text = "Leul";
+
+                    raspunsCorect =1;
+
+                    break;
+                case 5:
+                    label1.Text = "Intrebarea numarul 5: ";
+                    label2.Text = "Ce animal este cel mai bun prieten al omului? ";
+                    button1.Text = "Pisica";
+                    button2.Text = "Cainele";
+                    button3.Text = "Lupul";
+
+                    raspunsCorect = 2;
+                    break;
+                case 6:
+                    procent = (int)Math.Round((double)(scor * 100) / (intrebariTotale-1));
+                    label1.Text = "Procentajul de raspunsuri corecte este: " + procent + "%";
+                    label2.Text = "Ati raspuns la " + scor + " intrebari corect!";
+                    button1.Hide();
+                    button2.Text = "Incercati Din Nou";
+                    button3.Hide();
+                    raspunsCorect = 2;
+                    break;
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Joc;
+
+namespace Tema6Forms
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
+/*
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Joc;
+
+namespace Joc_Main
+{
+    class Joc
+    {
+        [Flags]
+        public enum NuStiuInca
+        {
+            Pisica = 1,
+            Melcul = 2,
+            Lupul = 4,
+            Cainele = 8,
+        };
+
+        public const NuStiuInca AnimaleCompanie = NuStiuInca.Pisica & NuStiuInca.Cainele;
+        static void Main(string[] args)
+        {
+            bool exit = false;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("S. Incepere Joc!");
+                Console.WriteLine("X. Exit");
+                Console.Write("Optiunea Dumneavoastra: ");
+                string optiune = Console.ReadLine();
+
+                switch (optiune.ToUpper())
+                {
+                    case "S":
+                        inceputJoc();
+                        break;
+                    case "X":
+                        exit = true;
+                        break;
+                    default:
+                        Console.WriteLine("Optiune inexistenta");
+                        break;
+                }
+            } while (!exit);
+        }
+
+        static void inceputJoc()
+        {
+            VerificareRaspunsuri Verificare = new VerificareRaspunsuri();
+
+            Console.Clear();
+
+            Console.WriteLine("Intrebarea 1: Care este cel mai mare animal?");
+
+            Console.WriteLine("a) Elefantul");
+            Console.WriteLine("b) Soimul");
+            Console.WriteLine("c) Girafa");
+
+            Console.Write("Raspunsul tau: ");
+            string answer1 = Console.ReadLine();
+
+
+            if (Verificare.VerifRaspuns("a", answer1)) //note to future self: compara "a" cu "a" nu "elefant" cu "a".
+            {
+                Console.WriteLine("Corect!");
+                Verificare.ScrieRezultat("Intrebarea 1: ", answer1);
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Gresit. Raspunsul corect era a, Elefantul.");
+                Console.ReadKey();
+            }
+
+            Console.Clear();
+
+            Console.WriteLine("Intrebarea 2: Care este cel mai rapid animal?");
+            Console.WriteLine("a) Ghepard");
+            Console.WriteLine("b) Leul");
+            Console.WriteLine("c) Elefantul");
+            Console.Write("Raspunsul tau: ");
+            string answer2 = Console.ReadLine();
+
+            if (Verificare.VerifRaspuns("a", answer2))
+            {
+                Console.WriteLine("Corect!");
+                Verificare.ScrieRezultat("Intrebarea 2", answer2);
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Gresit. Raspunsul corect era a, Ghepard.");
+                Console.ReadKey();
+            }
+            Console.Clear();
+            Console.WriteLine("Intrebarea 3: Care este cel mai incet animal?");
+            Console.WriteLine("a) Elefantul");
+            Console.WriteLine("b) Girafa");
+            Console.WriteLine("c) Melcul");
+            Console.Write("Raspunsul tau: ");
+            string answer3 = Console.ReadLine();
+
+            if (Verificare.VerifRaspuns("c", answer3))
+            {
+                Console.WriteLine("Corect!");
+                Verificare.ScrieRezultat("Intrebarea 3", answer3);
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Gresit. Raspunsul corect era c, Melcul.");
+                Console.ReadKey();
+            }
+
+            Console.Clear();
+            Console.WriteLine("Intrebarea 4: Care animal face Miau?");
+            Console.WriteLine("a) Pisica");
+            Console.WriteLine("b) Cainele");
+            Console.WriteLine("c) Vulpea");
+            Console.Write("Raspunsul tau: ");
+            string answer4 = Console.ReadLine();
+
+            if (Verificare.VerifRaspuns("a", answer4))
+            {
+                Console.WriteLine("Corect!");
+                Verificare.ScrieRezultat("Intrebarea 4", answer4);
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Gresit. Raspunsul corect era a, Melcul.");
+                Console.ReadKey();
+            }
+            Console.Clear();
+            Console.WriteLine("Intrebarea 5: Care animal este cel mai bun prieten al omului?");
+            Console.WriteLine("a) Lupul");
+            Console.WriteLine("b) Cainele");
+            Console.WriteLine("c) Calul");
+            Console.Write("Raspunsul tau: ");
+            string answer5 = Console.ReadLine();
+
+            if (Verificare.VerifRaspuns("b", answer5))
+            {
+                Console.WriteLine("Corect!");
+                Verificare.ScrieRezultat("Intrebarea 5", answer5);
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Gresit. Raspunsul corect era b, Cainele.");
+                Console.ReadKey();
+            }
+
+            Console.Clear();
+
+            Console.WriteLine("Intrebarea 6: Care dintre urmatoarele animale sunt animale de companie?");
+            Console.WriteLine("1. Pisica ");
+            Console.WriteLine("2. Melcul ");
+            Console.WriteLine("3. Lupul ");
+            Console.WriteLine("4. Cainele ");
+            Console.WriteLine("\nScrieti raspunsurile cu virgula intre ele (ex. Pisica,Cainele)");
+            do
+            {
+                string answer6 = Console.ReadLine();
+                string[] raspunsuri6 = new string[6];
+                raspunsuri6 = answer6.Split(',');
+                NuStiuInca Raspunsuri6 = 0;
+                NuStiuInca Raspunsuri6Parse;
+                for (int i = 0; i < raspunsuri6.Length; i++)
+                {
+                    if (Enum.TryParse(raspunsuri6[i], out Raspunsuri6Parse))
+                    {
+                        Raspunsuri6 &= Raspunsuri6Parse;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+
+                if (Raspunsuri6 == AnimaleCompanie)
+                {
+                    Console.WriteLine("Da sunt animale de companie?!");
+                    Verificare.VerifRaspuns("a", "a");
+                    break;
+
+                }
+                else
+                {
+                    Console.WriteLine("Raspunsul este gresit");
+                    break;
+                }
+            }
+            while (true);
+            Console.WriteLine($"\nFelicitari! Ai raspuns la {Verificare.Score} raspunsuri corect!");
+            Console.ReadKey();
+        }
+    }
+}
+*/
